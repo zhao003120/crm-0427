212b696f9ac235fb2dbf635f3cb26d111cf12230
zhao003120 <1206150621@qq.com>
2016-09-18 21:20:47 +0800

    Update Test01.java

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -6,9 +6,6 @@ public class Test01 {
 		System.out.println("hello github");
 		System.out.println("hello 回应");
 		System.out.println("update 01");
-		
-		System.out.println("123");
-		System.out.println("122");
 		System.out.println("33333");
 		System.out.println("bbbb");
 		System.out.println("ffffffffff");
