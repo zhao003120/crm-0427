3b9a018286955f30fe65e4b26854b78c3da2cee8
zhao003120 <zhao003120@qq.com>
2016-09-18 20:59:34 +0800

    aaaa

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -1,16 +1,14 @@
 package com.atguigu.github.test;
 
 public class Test01 {
-	
 	public static void main(String[] args) {
-		
 		System.out.println("hello 0427");
 		System.out.println("hello github");
 		System.out.println("hello 回应");
 		System.out.println("update 01");
 		System.out.println("123");
 		System.out.println("122");
+		
 		System.out.println("aaaa");
 	}
 }
-
